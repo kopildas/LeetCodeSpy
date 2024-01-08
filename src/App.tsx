0449c5174eca358e84ec7a20e4a84ca77430af63
kopildas <102140363+kopildas@users.noreply.github.com>
2024-01-08 19:11:05 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  setInterval(() => {
+    setCount(count + 1);
+}, 1000);
+useEffect(() => {
+  const img = document.createElement('img');
+  img.src = chrome.runtime.getURL('logo_32.png');
+  document.body.append(img);
+
+  
+}, [])
+
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
